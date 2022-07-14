c592686d48c71ed9e0ae5b32669c9b9f88f25a3c
reevancutinho <reevancutinho@gmail.com>
2022-07-15 01:38:50 +0530

    minor changes

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("Working fine...");
+  res.send("student mentor data...");
 });
 app.use("/student", studentRoute);
 app.use("/mentor", mentorRoute);
